82425174152de94dff99a6d6ac53b2dc35ebfcd4
PercyP <pparachevfx@gmail.com>
2025-02-28 03:58:25 -0500

    Initial commit

---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace AOL_Reborn.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public LoginViewModel()
+        {
+            // Constructor logic here
+        }
+    }
+}
---
+++ Views/LoginWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AOL_Reborn.Views
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SignInButton_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameBox.Text;
+            string password = PasswordBox.Password;
+
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+            {
+                MessageBox.Show("Please enter both username and password.", "Login Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            // Here, you can validate user credentials or pass them to LoginViewModel
+            MessageBox.Show($"Welcome, {username}!", "Login Successful", MessageBoxButton.OK, MessageBoxImage.Information);
+
+            // Navigate to Main Chat Window (not yet implemented)
+            MainWindow chatWindow = new MainWindow();
+            chatWindow.Show();
+            this.Close();
+        }
+
+
+    }
+}
